15f53ead885843c67880479046967f8012c400b6
madomaru <melondoka1502@gmail.com>
2024-01-29 17:09:52 +0900

    Initial Commit

---
+++ ImageWebSocket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageWebSocket
+//
+//  Created by 伊藤まどか on 2024/01/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageWebSocket/ImageWebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageWebSocketApp.swift
+//  ImageWebSocket
+//
+//  Created by 伊藤まどか on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct ImageWebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
