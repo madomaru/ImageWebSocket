ada1bc28f859fd88ad3b745c4bc4ba80cf20f0dd
madomaru <melondoka1502@gmail.com>
2024-02-05 19:54:37 +0900

    WebSocketでGlitchのサーバーに文字列を送れるようにした

---
+++ ImageWebSocket/View/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  ImageWebSocket
+//
+//  Created by 伊藤まどか on 2024/01/29.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ContentView: View {
+    @ObservedObject var client: WebSocketClient
+    @State private var selectedPhoto: PhotosPickerItem? = nil
+
+    init(){
+        client = WebSocketClient()
+        client.setup(url: "wss://websocket-image-server-toman.glitch.me")
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            if client.isConnected{
+                List{
+                    ForEach(client.messages, id: \.self){message in
+                        Text(message)
+                    }
+                }
+            }else {
+                Text("接続中")
+            }
+            Button(action: {
+                client.send("a")
+            }, label: {
+                Text("aと送る")
+                    .font(.title)
+            })
+            Spacer()
+            PhotosPicker(
+                selection: $selectedPhoto,
+                label: {
+                    Text("送る画像を選んでください")
+                        .font(.title)
+                }
+            )
+            Spacer()
+            Text(" ↓ Received Image")
+                .font(.title)
+            Image("SampleImg")
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal)
+            Spacer()
+        }
+        .padding()
+        .onAppear(){
+            client.connect()
+        }
+        .onDisappear(){
+            client.disconnect()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
