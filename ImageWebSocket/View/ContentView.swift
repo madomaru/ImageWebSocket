5bbd861bdc0e004edcda1020114847a656862753
madomaru <melondoka1502@gmail.com>
2024-02-07 15:56:28 +0900

    サーバーに画像を送る処理を追加

--- ImageWebSocket/View/ContentView.swift
+++ ImageWebSocket/View/ContentView.swift
@@ -11,8 +11,8 @@ import PhotosUI
 struct ContentView: View {
     @ObservedObject var client: WebSocketClient
     @State private var selectedPhoto: PhotosPickerItem? = nil
-    @State var selectedImage: UIImage?
-
+    @State var tmpImage: UIImage? = nil
+    
     init(){
         client = WebSocketClient()
         client.setup(url: "wss://websocket-image-server-toman.glitch.me")
@@ -23,11 +23,7 @@ struct ContentView: View {
             Spacer()
             // space to show recieve from server.
             if client.isConnected{
-                List{
-                    ForEach(client.messages, id: \.self){message in
-                        Text(message)
-                    }
-                }
+                Text("接続済み")
             }else {
                 Text("接続中")
             }
@@ -49,19 +45,41 @@ struct ContentView: View {
                         .font(.title)
                 }
             ).onChange(of: selectedPhoto){oldValue, pickedItem in
+                
                 Task {
+                    // PickedItem → UIImageに変換
                     guard let imageData = try await pickedItem?.loadTransferable(type: Data.self) else {return}
                     guard let uiImage = UIImage(data: imageData) else {return}
-                    selectedImage = uiImage
+                    
+                    // UIImage→Base64に変換
+                    guard let sendString = convertImageToBase64(uiImage) else {
+                        print("can't convert UIImage to Base64")
+                        return
+                    }
+                    guard let compressionImage = convertBase64ToImage(sendString) else {return}
+                    tmpImage = compressionImage
+                    
+//                    client.send(sendString)
                 }
+                
+                
+                
             }
             Spacer()
             Text(" ↓ Received Image")
                 .font(.title)
-            Image("SampleImg")
-                .resizable()
-                .scaledToFit()
-                .padding(.horizontal)
+            if tmpImage != nil {
+                Image(uiImage: tmpImage!)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal)
+            }else{
+                Image("SampleImg")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal)
+            }
+            
             Spacer()
         }
         .padding()
@@ -71,6 +89,18 @@ struct ContentView: View {
         .onDisappear(){
             client.disconnect()
         }
+    }
+    
+    // UIImage→Base64に変換するメソッド
+    private func convertImageToBase64(_ image: UIImage) -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 0.3) else { return nil }
+        return imageData.base64EncodedString()
+    }
+    
+    // Base64→UIImageに変換するメソッド
+    private func convertBase64ToImage(_ base64String: String) -> UIImage? {
+        guard let imageData = Data(base64Encoded: base64String) else { return nil }
+        return UIImage(data: imageData)
     }
 }
 
